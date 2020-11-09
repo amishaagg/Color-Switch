5b8bc3f229dd8e43c2e0b2c70720282584c07eda
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-09 19:19:22 +0530

    Add files via upload

---
+++ code/Ball.java
@@ -0,0 +1,17 @@
+public class Ball extends GameElement
+{
+    private int y;
+
+    public Ball(Player player)
+    {
+
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
