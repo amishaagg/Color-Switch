04549425d7c4622c65d962397fd1a0a139c40a86
amishaagg <55058841+amishaagg@users.noreply.github.com>
2020-11-09 20:12:56 +0530

    Update GameElement.java
    added getters and setters

--- code/GameElement.java
+++ code/GameElement.java
@@ -1,8 +1,32 @@
 public class GameElement
 {
-    int x;
-    int y;
-    int height;
-    int width;
+    private int x;
+    private int y;
+    private int height;
+    public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	int width;
 
 }
