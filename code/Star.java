11362fd14c8a1ae3126cb8c9aca0f88048b86567
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-09 23:20:10 +0530

    Update Star.java
    made the variable 'value' private and added getter/setter methods.

--- code/Star.java
+++ code/Star.java
@@ -1,4 +1,12 @@
 public class Star extends GameElement
 {
-    int value;
+    private int value;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
