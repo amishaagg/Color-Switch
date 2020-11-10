ba655db93618ff9b98fbed34f432835c19d4e21f
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-10 17:25:31 +0530

    Update MainMenu.java
    added newGame(), resumeGame(), exitGame(), List<Game> savedGames.

--- code/MainMenu.java
+++ code/MainMenu.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 public class MainMenu
 {
+    private static ArrayList<Game> savedGames;
 
+    public static ArrayList<Game> getSavedGames() {
+        return savedGames;
+    }
+
+    public static void setSavedGames(ArrayList<Game> savedGames) {
+        MainMenu.savedGames = savedGames;
+    }
+
+    public void saveGame(){};
+    public void newGame(){};
+    public void exitGame(){};
 }
