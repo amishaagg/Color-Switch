f76d812f47b80ab08e32f5a7a37e6c1fa00fcd3e
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-17 00:20:18 +0530

    Update MainMenu.java

--- code/MainMenu.java
+++ code/MainMenu.java
@@ -1,9 +1,28 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class MainMenu
+public class MainMenu extends Application implements Serializable
 {
+    Button btnNewGame, btnResumeGame, btnExitGame, btnreturn;
+    Scene scene1, scene2;
+    Stage window;
     private static ArrayList<Game> savedGames;
-
     public static ArrayList<Game> getSavedGames() {
         return savedGames;
     }
@@ -12,7 +31,92 @@ public class MainMenu
         MainMenu.savedGames = savedGames;
     }
 
+
     public void saveGame(){};
-    public void newGame(){};
-    public void exitGame(){};
+    public void newGame(Stage stage) throws FileNotFoundException
+    {
+        window = stage;
+        window.setTitle("Color Switch");
+        window.setOnCloseRequest(e->{
+            e.consume();
+            exitGame();
+        });
+        btnNewGame=new Button("New Game");
+        btnResumeGame=new Button("Resume");
+        btnExitGame=new Button("Exit Game");
+        btnreturn=new Button("Go back to main menu");
+        Button b = new Button();
+        btnNewGame.setOnAction(e->
+        {
+            new Game(window);
+        });
+        btnResumeGame.setOnAction(e->resumeGame());
+        btnExitGame.setOnAction(e->exitGame());
+        btnreturn.setOnAction(e->window.setScene(scene1));
+
+        Image image = new Image(new FileInputStream("assets/MainMenuImage.jpg"));
+        ImageView imageView = new ImageView(image);
+        imageView.setX(-5);
+        imageView.setY(0);
+        imageView.setFitHeight(400);
+        imageView.setFitWidth(600);
+        window.setHeight(400);
+        window.setWidth(600);
+
+        Group layout1=new Group(imageView);
+        layout1.getChildren().addAll(btnNewGame,btnResumeGame,btnExitGame);
+        btnNewGame.setPrefWidth(80);
+        btnNewGame.setLayoutX(245);
+        btnNewGame.setLayoutY(250);
+        btnResumeGame.setPrefWidth(80);
+        btnResumeGame.setLayoutX(245);
+        btnResumeGame.setLayoutY(280);
+        btnExitGame.setPrefWidth(80);
+        btnExitGame.setLayoutX(245);
+        btnExitGame.setLayoutY(310);
+        scene1=new Scene(layout1,300,300);
+        Label label=new Label("Yo I am list of games");
+        VBox layout2=new VBox(20);
+        layout2.setAlignment(Pos.CENTER);
+        layout2.getChildren().addAll(label,btnreturn);
+        scene2=new Scene(layout2,250,250);
+        window.setScene(scene1);
+        window.show();
+    };
+    public void exitGame()
+    {
+        window.close();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        newGame(stage);
+    }
+
+    public static void display(String title,String message){
+        Stage window=new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+
+        Label label=new Label(message);
+        Button closeButton=new Button("Return to game");
+        closeButton.setOnAction(e->window.close());
+
+        VBox layout=new VBox(20);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(label,closeButton);
+
+        Scene scene=new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+    public void resumeGame(){
+        window.setScene(scene2);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
