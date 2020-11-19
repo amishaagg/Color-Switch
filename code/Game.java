84af4e2e5ad8b3e1d7dd7e767e17abc9f2ad406f
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-20 01:53:44 +0530

    Update Game.java

--- code/Game.java
+++ code/Game.java
@@ -4,15 +4,19 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Game
 {
@@ -63,28 +67,37 @@ public class Game
     }
 
     Button btnPause;
-    public Game(Stage window)
+    Stage window;
+    public Game(Stage primaryStage) throws FileNotFoundException
     {
+        window = new Stage();
         Group layout = new Group();
         Scene scene = new Scene(layout);
         window.setWidth(300);
         window.setHeight(650);
-        btnPause = new Button("||");
+        btnPause = new Button();
+        Image image = new Image(new FileInputStream("assets/Pause_BTN.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setX(5);
+        imageView.setY(5);
+        imageView.setFitHeight(40);
+        imageView.setFitWidth(40);
+        btnPause.setGraphic(imageView);
+        btnPause.setBackground(Background.EMPTY);
         btnPause.setLayoutX(10);
         btnPause.setLayoutY(10);
         layout.getChildren().add(btnPause);
-        btnPause.setOnAction(e-> pause(window));
-        scene.setFill(Color.BLACK);
+        btnPause.setOnAction(e-> pause(window, primaryStage));
+        scene.setFill(Color.rgb(40,40,40));
         window.setScene(scene);
         window.show();
     }
 
-    public void pause(Stage GameStage)
+    public void pause(Stage GameStage, Stage primaryStage)
     {
         Stage window=new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
-        HBox layout = new HBox(25);
-        layout.setPadding(new Insets(10,10,10,10));
+        Group layout = new Group();
         window.setTitle("PAUSE");
         Button resume = new Button("RESUME");
         Button exit = new Button("EXIT");
@@ -94,15 +107,15 @@ public class Game
         resume.setOnAction(e->window.close());
         exit.setOnAction(e->
         {
-            MainMenu mainMenu = new MainMenu();
-            try {
-                mainMenu.newGame(GameStage);
-            } catch (FileNotFoundException fileNotFoundException) {
-                fileNotFoundException.printStackTrace();
-            }
+            primaryStage.show();
+            GameStage.close();
             window.close();
         });
+        resume.setLayoutX(10);
+        save.setLayoutX(90);
+        exit.setLayoutX(170);
         Scene scene = new Scene(layout, 220, 100);
+        scene.setFill(Color.rgb(40,40,40));
         window.setScene(scene);
         window.showAndWait();
     }
