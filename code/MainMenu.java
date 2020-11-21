1edf7a6644068efe000e607bd83933dd962a93d0
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-22 02:16:01 +0530

    Update MainMenu.java

--- code/MainMenu.java
+++ code/MainMenu.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import java.nio.file.Paths;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -10,13 +11,22 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import javax.print.DocFlavor;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainMenu extends Application implements Serializable
@@ -24,6 +34,7 @@ public class MainMenu extends Application implements Serializable
     Button btnNewGame, btnResumeGame, btnExitGame, btnreturn;
     Scene scene1, scene2;
     Stage window;
+    Media media;
     private static ArrayList<Game> savedGames;
     public static ArrayList<Game> getSavedGames() {
         return savedGames;
@@ -39,25 +50,30 @@ public class MainMenu extends Application implements Serializable
     {
         window = stage;
         window.setTitle("Color Switch");
-        window.setOnCloseRequest(e->{
+        window.setOnCloseRequest(e->
+        {
             e.consume();
             exitGame();
         });
         btnNewGame=new Button();
         Image play = new Image(new FileInputStream("assets/play.png"));
-        ImageView imageViewplay = new ImageView(play);
-        imageViewplay.setFitWidth(80);
-        imageViewplay.setFitHeight(80);
-        btnNewGame.setGraphic(imageViewplay);
+        ImageView imageViewPlay = new ImageView(play);
+        imageViewPlay.setFitWidth(80);
+        imageViewPlay.setFitHeight(80);
+        btnNewGame.setGraphic(imageViewPlay);
         btnNewGame.setBackground(Background.EMPTY);
         btnResumeGame=new Button();
         Image resume = new Image(new FileInputStream("assets/resume.png"));
         ImageView imageViewResume = new ImageView(resume);
         btnResumeGame.setGraphic(imageViewResume);
         btnResumeGame.setBackground(Background.EMPTY);
-        btnExitGame=new Button("Exit Game");
-        btnreturn=new Button("Go back to main menu");
-        Button b = new Button();
+        btnExitGame=new Button();
+        Image exit = new Image(new FileInputStream("assets/exit.png"));
+        ImageView imageViewExit = new ImageView(exit);
+        imageViewExit.setFitHeight(40);
+        imageViewExit.setFitWidth(40);
+        btnExitGame.setGraphic(imageViewExit);
+        btnExitGame.setBackground(Background.EMPTY);
         btnNewGame.setOnAction(e->
         {
             window.hide();
@@ -67,9 +83,14 @@ public class MainMenu extends Application implements Serializable
                 fileNotFoundException.printStackTrace();
             }
         });
-        btnResumeGame.setOnAction(e->resumeGame());
+        btnResumeGame.setOnAction(e-> {
+            try {
+                resumeGame();
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+        });
         btnExitGame.setOnAction(e->exitGame());
-        btnreturn.setOnAction(e->window.setScene(scene1));
 
         Image image = new Image(new FileInputStream("assets/mainmenu.png"));
         ImageView imageView = new ImageView(image);
@@ -79,10 +100,7 @@ public class MainMenu extends Application implements Serializable
         imageView.setFitWidth(400);
         window.setHeight(700);
         window.setWidth(400);
-        System.out.println(image.getHeight());
-        System.out.println(image.getWidth());
         Group layout1 = new Group(imageView);
-        //layout1.getChildren().add(btnNewGame);
         layout1.getChildren().addAll(btnNewGame,btnResumeGame,btnExitGame);
         btnNewGame.setPrefWidth(80);
         btnNewGame.setLayoutX(160);
@@ -91,14 +109,9 @@ public class MainMenu extends Application implements Serializable
         btnResumeGame.setLayoutX(100);
         btnResumeGame.setLayoutY(480);
         btnExitGame.setPrefWidth(80);
-        btnExitGame.setLayoutX(250);
-        btnExitGame.setLayoutY(310);
+        btnExitGame.setLayoutX(300);
+        btnExitGame.setLayoutY(70);
         scene1=new Scene(layout1,300,300);
-        Label label=new Label("Yo I am list of games");
-        VBox layout2=new VBox(20);
-        layout2.setAlignment(Pos.CENTER);
-        layout2.getChildren().addAll(label,btnreturn);
-        scene2=new Scene(layout2,250,250);
         window.setScene(scene1);
         window.show();
     };
@@ -113,26 +126,25 @@ public class MainMenu extends Application implements Serializable
         newGame(stage);
     }
 
-    public static void display(String title,String message){
-        Stage window=new Stage();
-        window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle(title);
-
-        Label label=new Label(message);
-        Button closeButton=new Button("Return to game");
-        closeButton.setOnAction(e->window.close());
-
-        VBox layout=new VBox(20);
-        layout.setAlignment(Pos.CENTER);
-        layout.getChildren().addAll(label,closeButton);
-
-        Scene scene=new Scene(layout);
-        window.setScene(scene);
-        window.showAndWait();
-    }
-
-    public void resumeGame(){
+    public void resumeGame() throws FileNotFoundException {
+        btnreturn=new Button();
+        Image home = new Image(new FileInputStream("assets/resume_home.png"));
+        ImageView imageViewHome = new ImageView(home);
+        imageViewHome.setFitWidth(50);
+        imageViewHome.setFitHeight(50);
+        btnreturn.setGraphic(imageViewHome);
+        btnreturn.setBackground(Background.EMPTY);
+        btnreturn.setLayoutX(10);
+        Image choose = new Image(new FileInputStream("assets/CHOOSE_GAME.png"));
+        ImageView imageViewChoose = new ImageView(choose);
+        //imageViewChoose.setFitHeight(80);
+        //imageViewChoose.setFitWidth(400);
+        Group layout2=new Group();
+        layout2.getChildren().addAll(imageViewChoose, btnreturn);
+        scene2=new Scene(layout2,250,250);
+        scene2.setFill(Color.rgb(40,40,40));
         window.setScene(scene2);
+        btnreturn.setOnAction(e->window.setScene(scene1));
     }
 
     public static void main(String[] args) {
