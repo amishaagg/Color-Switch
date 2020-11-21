10c8b0045e177afbd999e8aac3e5e8a090690f90
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-22 02:18:39 +0530

    Update Obstacle.java

--- code/Obstacle.java
+++ code/Obstacle.java
@@ -1,13 +1,37 @@
+import javafx.scene.Group;
+import java.util.ArrayList;
+
 public class Obstacle extends GameElement
 {
-    double speed;
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
+    private double speed;
+    private ArrayList<String> colors;
+    private Group obstacleGroup;
 
+    public Obstacle(Group obstacleGroup)
+    {
+        this.obstacleGroup = obstacleGroup;
+    }
     public double getSpeed() {
         return speed;
     }
 
-    public void move(){}
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(ArrayList<String> colors) {
+        this.colors = colors;
+    }
+
+    public Group getObstacleGroup() {
+        return obstacleGroup;
+    }
+
+    public void setObstacleGroup(Group obstacleGroup) {
+        this.obstacleGroup = obstacleGroup;
+    }
 }
