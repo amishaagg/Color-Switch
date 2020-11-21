cec661dd185c6dfe2d36b5d349bc336dca06dc1f
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-11-22 02:16:35 +0530

    Update GameElement.java

--- code/GameElement.java
+++ code/GameElement.java
@@ -4,30 +4,16 @@ public class GameElement
     private int y;
     private int height;
     private int width;
+
+    public int getY() {
+        return y;
+    }
+
     public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	public int getHeight() {
-		return height;
-	}
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	public int getWidth() {
-		return width;
-	}
-	public void setWidth(int width) {
-		this.width = width;
-	}
-	
+        return x;
+    }
+
+    public void rotate()
+    {}
 
 }
