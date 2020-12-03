69595bc140b09cd804abe0ee1157f55acdb06a38
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-12-04 02:40:40 +0530

    Update ColorSwitcher.java

--- code/ColorSwitcher.java
+++ code/ColorSwitcher.java
@@ -9,5 +9,13 @@ public class ColorSwitcher
         this.colorSwitcher = colorSwitcher;
     }
 
+    public ImageView getColorSwitcher() {
+        return colorSwitcher;
+    }
+
+    public void setColorSwitcher(ImageView colorSwitcher) {
+        this.colorSwitcher = colorSwitcher;
+    }
+
     public static void changeColor(Ball ball){};
 }
