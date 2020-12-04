2a0abd832426e5c092af1ba176f252042d993f7a
KingYashTanwar <73228640+KingYashTanwar@users.noreply.github.com>
2020-12-04 18:47:17 +0530

    Update ColorSwitcher.java
    added colorChange method.

--- code/ColorSwitcher.java
+++ code/ColorSwitcher.java
@@ -1,4 +1,7 @@
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
+import java.util.Random;
 
 public class ColorSwitcher
 {
@@ -17,5 +20,59 @@ public class ColorSwitcher
         this.colorSwitcher = colorSwitcher;
     }
 
-    public static void changeColor(Ball ball){};
+    public static void changeColor(Ball ball)
+    {
+        Random rand = new Random();
+        int color = rand.nextInt(4);
+        switch (color)
+        {
+            case 0: if(ball.getColor().equals("yellow")) //yellow
+                    {
+                        ball.getCircle().setFill(Color.rgb(144,13,255));
+                        ball.setColor("purple");
+                    }
+                    else
+                    {
+                        ball.getCircle().setFill(Color.rgb(250,225,0));
+                        ball.setColor("yellow");
+                    }
+                    break;
+
+            case 1: if(ball.getColor().equals("cyan")) //cyan
+                    {
+                        ball.getCircle().setFill(Color.rgb(250,225,0));
+                        ball.setColor("yellow");
+                    }
+                    else
+                    {
+                        ball.getCircle().setFill(Color.rgb(50,219,240));
+                        ball.setColor("cyan");
+                    }
+                    break;
+
+            case 2: if(ball.getColor().equals("purple")) //purple
+                    {
+                        ball.getCircle().setFill(Color.rgb(255,1,129));
+                        ball.setColor("pink");
+                    }
+                    else
+                    {
+                        ball.getCircle().setFill(Color.rgb(144,13,225));
+                        ball.setColor("purple");
+                    }
+                    break;
+
+            case 3: if(ball.getColor().equals("pink")) //pink
+                    {
+                        ball.getCircle().setFill(Color.rgb(50,219,240));
+                        ball.setColor("cyan");
+                    }
+                    else
+                    {
+                        ball.getCircle().setFill(Color.rgb(255,1,129));
+                        ball.setColor("pink");
+                    }
+                    break;
+        }
+    }
 }
